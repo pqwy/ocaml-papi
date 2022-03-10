786c9cbdc7a24669eebb9f0a5e2a5c9416a55e10
Kate <kit.ty.kate@disroot.org>
2022-03-10 12:30:45 +0000

    Upgrade to OCaml 4.14

--- src/papi_top.ml
+++ src/papi_top.ml
@@ -1,4 +1,8 @@
 (* Copyright (c) 2019 David Kaloper Meršinjak. All rights reserved.
    See LICENSE.md *)
 
+#if OCAML_VERSION >= (4,14,0)
+let _ = Toploop.use_silently Format.err_formatter (Toploop.File "papi_top_init.ml")
+#else
 let _ = Toploop.use_silently Format.err_formatter "papi_top_init.ml"
+#endif
